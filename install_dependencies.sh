49914b541a80b84ed778795b652a1ba976f6377f
583058 <solomonnathaniel@C02S50MXG8WM.local>
2019-03-31 03:40:10 -0400

    update config

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo $REGISTRY_PASS
+
 jspm config registries.github.remote https://github.jspm.io
 jspm config registries.github.auth $REGISTRY_PASS
 jspm config registries.github.maxRepoSize 1000
