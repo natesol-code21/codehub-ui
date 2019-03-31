662ceb1821ea2a8e96a904f2a8aaa553d8d53012
583058 <solomonnathaniel@C02S50MXG8WM.local>
2019-03-30 22:51:16 -0400

    config update

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #JSPM_GITHUB_AUTH_TOKEN='bmF0ZXNvbC1jb2RlMjE6TWVzczE5NTUlMjMlMjMlNDAlNDA='
-JSPM_GITHUB_AUTH_TOKEN='ff3f998394457f40ce21ecbd9a6bbb4e9cbddbda'
+JSPM_GITHUB_AUTH_TOKEN='239c12e8e6336b1e717bb49f316cff6c95d34091'
 jspm config registries.github.remote https://github.jspm.io
 jspm config registries.github.auth $JSPM_GITHUB_AUTH_TOKEN
 jspm config registries.github.maxRepoSize 1000
