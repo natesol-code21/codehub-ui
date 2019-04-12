5b57b7664a5c1255139a14e7230be6ec2977fe6c
583058 <solomonnathaniel@C02S50MXG8WM.local>
2019-04-12 01:43:10 -0400

    config updates

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apt-get install python-yaml
+apt-get install python-yaml -y
 jspm config registries.github.repo jspm-registry
 jspm config registries.github.auth $JSPM_GITHUB_AUTH_TOKEN
 jspm config registries.github.maxRepoSize 10000000
