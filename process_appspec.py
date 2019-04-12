be589eca32789a85a51e0b3de2d0fceda7d22775
583058 <solomonnathaniel@C02S50MXG8WM.local>
2019-04-12 00:31:23 -0400

    config updates

--- process_appspec.py
+++ process_appspec.py
@@ -1,19 +1,12 @@
+#!/usr/bin/env python
 import yaml
 import os
 def process_version():
-  print("Hello World!")
-  #res=subprocess.check_call("aws ecs list-task-definitions --family-prefix codehub-ui | jq -r '.taskDefinitionArns[-1]'",shell=True)
-  #print(res)
   with open('appspec.yaml', 'r') as f:
       doc=yaml.safe_load(f)
-      #print(doc)
-      #res=call("aws ecs list-task-definitions --family-prefix codehub-ui | jq -r '.taskDefinitionArns[-1]'",shell=True)
       doc["Resources"][0]["TargetService"]["Properties"]["TaskDefinition"]=os.environ['CODEHUB_UI_TASK_VERSION']
-      print(yaml.dump(doc))
       with open('appspec.yaml', 'w') as yaml_file:
          yaml.dump(doc, yaml_file, default_flow_style=False)
-
-
 
 if __name__== "__main__":
   process_version()
